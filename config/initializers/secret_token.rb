02d91006794d84d88361e7c219e86609ad1e16db
Amos Stone <amos@amosarts.com>
2013-11-08 09:43:23 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skillmap::Application.config.secret_key_base = 'b8cd5beca1826cec5569f6bef4f3fa5aeeb11354ab619d0254ade9311445f4e314b5be65e7f3961697f0623509c498caeb53497187e81a27e7f1c246498098d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Skillmap::Application.config.session_store :cookie_store, key: '_skillmap_session'
